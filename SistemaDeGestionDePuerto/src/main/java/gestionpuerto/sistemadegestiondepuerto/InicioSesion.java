64dc384323378dd4f3693fd9f57465d3985bc287
Esteban <esteban.qr2016000@gmail.com>
2024-01-20 01:47:52 -0500

    Inicio de sesion y menu de inicio de la plataforma

---
+++ SistemaDeGestionDePuerto/src/main/java/gestionpuerto/sistemadegestiondepuerto/InicioSesion.java
@@ -0,0 +1,37 @@
+package gestionpuerto.sistemadegestiondepuerto;
+
+import java.util.Scanner;
+
+public class InicioSesion {
+    private String usuario;
+    private String contraseña;
+    private Scanner scanner;
+    private boolean resp;
+
+    public InicioSesion(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+  
+        public boolean iniciarSesion(){
+
+            System.out.println("Iniar Sesion");
+            System.out.println("Usuario: ");
+            usuario=scanner.nextLine();
+            System.out.println("Contraseña: ");
+            contraseña=scanner.nextLine();
+               if (usuario.equals("Esteban") && contraseña.equals("123")) {
+                   resp = true;
+               } else {
+                   resp = false;
+               }
+
+            return resp; 
+        }
+
+    public String getUsuario() {
+        return usuario;
+    }
+ 
+    
+}
--- SistemaDeGestionDePuerto/src/main/java/gestionpuerto/sistemadegestiondepuerto/SistemaDeGestionDePuerto.java
+++ SistemaDeGestionDePuerto/src/main/java/gestionpuerto/sistemadegestiondepuerto/SistemaDeGestionDePuerto.java
@@ -6,35 +6,44 @@ import java.util.Scanner;
 public class SistemaDeGestionDePuerto {
 
     public static void main(String[] args) {
-          Scanner scanner = new Scanner(System.in);
-        int opcion;
+        Scanner scanner = new Scanner(System.in);
+        InicioSesion inicioSesion;
 
         do {
-            mostrarMenu();
-            System.out.print("Selecciona una opción: ");
-            opcion = scanner.nextInt();
+            inicioSesion = new InicioSesion(scanner); 
+            if (inicioSesion.iniciarSesion()) {
+                System.out.println("Inicio de sesión exitoso. ¡Bienvenido, " + inicioSesion.getUsuario() + "!");
 
-            switch (opcion) {
-                case 1:
-                    System.out.println("Opción 1 ");
-                    break;
-                case 2:
-                    System.out.println("Opción 2 ");
-                    break;
-                case 3:
-                    System.out.println("Opción 3 ");
-                    break;
-                case 4:
-                    System.out.println("Saliendo del programa. ¡Hasta luego!");
-                    break;
-                default:
-                    System.out.println("Opción no válida. Inténtalo de nuevo.");
+                int opcion;
+                do {
+                    // Menú principal
+                    mostrarMenu();
+                    System.out.println("Ingresar una opción: ");
+                    opcion = scanner.nextInt();
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Opción 1");
+                            break;
+                        case 2:
+                            System.out.println("Opción 2");
+                            break;
+                        case 3:
+                            System.out.println("Opción 3");
+                            break;
+                        case 4:
+                            System.out.println("Cerrando sesión. ¡Hasta luego, " + inicioSesion.getUsuario() + "!");
+                            break;
+                        default:
+                            System.out.println("Opción Incorrecta, intente de nuevo");
+                    }
+                } while (opcion != 4);
+            } else {
+                System.out.println("Usuario y/o contraseña incorrecta, intente nuevamente....");
             }
 
-        } while (opcion != 4);
-
-        scanner.close();
+        } while (true); // Sigue solicitando credenciales mientras no haya iniciado sesión
     }
+
 
     public static void mostrarMenu() {
         System.out.println("\n--- Menú de Opciones ---");
