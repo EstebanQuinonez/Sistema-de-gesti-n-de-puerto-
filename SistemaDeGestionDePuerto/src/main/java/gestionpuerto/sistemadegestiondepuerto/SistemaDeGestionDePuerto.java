4c3fbc45027b6a007ca8e586ee6c037bcd94ccb2
EstebanQuiñonez <esteban.quinonez@unmsm.edu.pe>
2024-01-22 02:13:00 -0500

    Funcionalidades Basicas

--- SistemaDeGestionDePuerto/src/main/java/gestionpuerto/sistemadegestiondepuerto/SistemaDeGestionDePuerto.java
+++ SistemaDeGestionDePuerto/src/main/java/gestionpuerto/sistemadegestiondepuerto/SistemaDeGestionDePuerto.java
@@ -31,12 +31,18 @@ public class SistemaDeGestionDePuerto {
                             System.out.println("Opción 3");
                             break;
                         case 4:
+                            System.out.println("Opción 4");
+                            break;
+                        case 5:
+                            System.out.println("Opción 5");
+                            break;
+                        case 6:
                             System.out.println("Cerrando sesión. ¡Hasta luego, " + inicioSesion.getUsuario() + "!");
                             break;
                         default:
                             System.out.println("Opción Incorrecta, intente de nuevo");
                     }
-                } while (opcion != 4);
+                } while (opcion != 6);
             } else {
                 System.out.println("Usuario y/o contraseña incorrecta, intente nuevamente....");
             }
@@ -47,10 +53,12 @@ public class SistemaDeGestionDePuerto {
 
     public static void mostrarMenu() {
         System.out.println("\n--- Menú de Opciones ---");
-        System.out.println("1. Realizar tarea 1");
-        System.out.println("2. Realizar tarea 2");
-        System.out.println("3. Realizar tarea 3");
-        System.out.println("4. Salir");
+        System.out.println("1. Registro y Gestión de Buques");
+        System.out.println("2. Registro y Gestión de Cargas");
+        System.out.println("3. Planificación de Atraques");
+        System.out.println("4. Gestión de Empleados");
+        System.out.println("5. Facturación y Finanzas");
+        System.out.println("6. Salir del Sistema");
     }
     }
 
