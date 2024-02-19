23617fa2837e297ec4d8a8cec20dbc19e95dbc9f
Alex Villanueva <villanuevaaguirre876@gmail.com>
2024-02-19 11:27:59 -0500

    Create SistemaDeGestionDePuerto.java

---
+++ SistemaDeGestionDePuerto/src/main/java/com/mycompany/sistemadegestiondepuerto/SistemaDeGestionDePuerto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemadegestiondepuerto;
+
+/**
+ *
+ * @author villa
+ */
+public class SistemaDeGestionDePuerto {
+
+    public static void main(String[] args) {
+        InterfazLogin objetoLogin = new InterfazLogin();
+        objetoLogin.setVisible(true);
+    }
+}
