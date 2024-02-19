5169b133b7bdfb40d55e921c773919c34a72b3c1
Alex Villanueva <villanuevaaguirre876@gmail.com>
2024-02-19 11:31:06 -0500

    Update CConexion.java

--- SistemaDeGestionDePuerto/src/main/java/Clases/CConexion.java
+++ SistemaDeGestionDePuerto/src/main/java/Clases/CConexion.java
@@ -16,10 +16,10 @@ public class CConexion {
     Connection conectar;
     
     String usuario = "root";
-    String contraseña = "GHMySQL22.11";
+    String contraseña = "123456";
     String bd = "SistemaDeGestionDePuerto";
     String ip = "localhost";
-    String puerto = "3307";
+    String puerto = "3306";
     
     String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + bd;
     
